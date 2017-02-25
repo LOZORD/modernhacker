4935625735f20059bc5290a79ba309813d951af8
LOZORD <ljrudberg@gmail.com>
2017-02-25 17:45:42 -0500

    Being more evil++

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,16 @@ export class AppComponent implements OnInit {
   ready = false;
   private footer: HTMLElement;
   snippets: { code: string, color: string }[] = [];
+  gotSegFault = false;
   constructor(private service: AppService) { }
 
   ngOnInit() {
-    this.service.getCode().subscribe(code => {
+    const codeUrls = [
+      'https://raw.githubusercontent.com/LOZORD/xanadu/master/src/game/actions.ts',
+      'https://raw.githubusercontent.com/torvalds/linux/master/net/batman-adv/distributed-arp-table.c',
+      'https://raw.githubusercontent.com/c00kiemon5ter/ioccc-obfuscated-c-contest/master/2013/morgan1/morgan1.c'
+    ];
+    this.service.getCode(AppComponent.getRandomArrayValue(codeUrls)).subscribe(code => {
       this.modelCode += code.trim();
     });
   }
@@ -27,46 +33,60 @@ export class AppComponent implements OnInit {
     this.footer = document.getElementById('footer');
   }
 
-  @HostListener('document:keyup', ['$event'])
+  @HostListener('document:keydown', ['$event'])
   onKeyPress(event: KeyboardEvent): void {
+    if (this.gotSegFault) {
+      this.ready = false;
+      alert('Well this is shit no one needs...')
+      setTimeout(() => location.reload(), 3000)
+    }
     if (this.ready) {
       if (this.typeIndex < this.modelCode.length) {
         const ff = AppComponent.getRandomGroupingNumber();
-        const c = AppComponent.getColor(ff);
+        const c = AppComponent.getColor();
         const s = AppComponent.rawToHTML(this.modelCode.slice(this.typeIndex, this.typeIndex + ff));
         this.typeIndex += ff;
         this.snippets.push({
           code: s,
           color: c
         });
-        this.beEvil();
+        if (this.typeIndex > 0) {
+          this.beEvil();
+        }
         this.footer.scrollIntoView(false);
       }
     }
   }
 
   beEvil(): void {
-    if (this.typeIndex % 250 === 0) {
-      switch (AppComponent.getRandomGroupingNumber()) {
-        case 1:
-          this.openWindow('https://news.ycombinator.com', false);
-          return;
-        case 2:
-          this.openWindow('https://www.soylent.com/products/', true)
-          return;
-        case 3:
-          this.openWindow('https://stupidhackathon.com/', true)
-          return;
-        case 4:
-          this.openWindow('https://www.google.com/#safe=active&q=where+can+i+buy+teledildonics&*', true)
-          return;
-        case 5:
-          this.openWindow('https://www.reddit.com/r/VaporwaveAesthetics/', true)
-          return;
-      }
+    if (this.typeIndex % 200 === 0) {
+      const urls = [
+        'https://news.ycombinator.com',
+        'https://www.soylent.com/products/',
+        'https://stupidhackathon.com/',
+        'https://www.google.com/#safe=active&q=where+can+i+buy+teledildonics&*',
+        'https://www.reddit.com/r/VaporwaveAesthetics/',
+        'https://www.google.com/#safe=active&q=should+i+use+haskell&*',
+        'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/14572845_1133764806716757_5142509850974282045_n.png?oh=54374a19491c65c8dc748cc509f39956&oe=59712EF8',
+        // Let's get meta...
+        window.location.href
+      ]
+
+      const url = AppComponent.getRandomArrayValue(urls);
+
+      this.openWindow(url, true);
     }
+
     if (this.typeIndex % 420 === 0) {
-      this.openWindow('http://gentlecuts.net/wp-content/uploads/2012/01/%E3%83%AA%E3%82%B5%E3%83%95%E3%83%A9%E3%83%B3%E3%82%AF420-_-%E7%8F%BE%E4%BB%A3%E3%81%AE%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC.mp3', false)
+      this.openWindow('http://gentlecuts.net/wp-content/uploads/2012/01/%E3%83%AA%E3%82%B5%E3%83%95%E3%83%A9%E3%83%B3%E3%82%AF420-_-%E7%8F%BE%E4%BB%A3%E3%81%AE%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC.mp3', true)
+    }
+
+    if (this.typeIndex % 500 === 0 && Math.random() > 0.90) {
+      this.snippets.push({
+        code: 'Seg fault\nCore dumped',
+        color: 'limegreen'
+      });
+      this.gotSegFault = true;
     }
   }
 
@@ -99,24 +119,24 @@ export class AppComponent implements OnInit {
     return s.replace(/(?:\r\n|\r|\n)/g, '<br/>').replace(' ', '&nbsp;');
   }
 
-  static getRandomGroupingNumber(max = 5): number {
-    return Math.round(Math.random() * max);
+  static getRandomArrayValue<T>(arr: T[]): T {
+    return arr[Math.floor(Math.random() * arr.length)];
   }
 
-  static getColor(n: number): string {
-    switch (n) {
-      case 1:
-        return 'cornflowerblue';
-      case 2:
-        return 'coral';
-      case 3:
-        return 'palevioletred';
-      case 4:
-        return 'white';
-      case 5:
-        return 'limegreen';
-      default:
-        return 'limegreen';
-    }
+  static getRandomGroupingNumber(max = 5): number {
+    return Math.floor(Math.random() * max);
+  }
+
+  static getColor(): string {
+    const colors = [
+      'cornflowerblue',
+      'coral',
+      'palevioletred',
+      'white',
+      'limegreen',
+      'gold'
+    ];
+
+    return AppComponent.getRandomArrayValue(colors);
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,5 @@
-import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
@@ -8,9 +8,8 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AppService {
-    defaultLink = 'https://raw.githubusercontent.com/LOZORD/xanadu/master/src/game/actions.ts';
-    constructor(private http: Http) {}
-    getCode(link = this.defaultLink): Observable<string> {
+    constructor(private http: Http) { }
+    getCode(link: string): Observable<string> {
         return this.http.get(link).map(resp => resp.text());
     }
 }
